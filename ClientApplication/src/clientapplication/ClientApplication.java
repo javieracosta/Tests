0ee292de5dc86d67b639f6e99d33965513396c6e
Javier Acosta <javiacosta@gmail.com>
2012-05-05 14:38:50 -0300

    Commit del tutorial

---
+++ ClientApplication/src/clientapplication/ClientApplication.java
@@ -0,0 +1,44 @@
+/*
+ * ClientApplication.java
+ */
+
+package clientapplication;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class ClientApplication extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new ClientView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of ClientApplication
+     */
+    public static ClientApplication getApplication() {
+        return Application.getInstance(ClientApplication.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(ClientApplication.class, args);
+    }
+}
